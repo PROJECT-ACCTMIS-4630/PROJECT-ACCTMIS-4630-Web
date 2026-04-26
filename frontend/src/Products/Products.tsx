04e55c2ca922dfd6f45565c554ee2e8de16ae1eb
sameerlatif858-glitch <sameerlatif858@gmail.com>
2026-04-26 19:48:46 -0400

    Update Products.tsx

--- frontend/src/Products/Products.tsx
+++ frontend/src/Products/Products.tsx
@@ -32,7 +32,7 @@ function Products() {
     <div className="content">
       <ul className="products">
         {products.map((product) => (
-          <li>
+          <li key ={product.id}>
             <div className="product">
               <img
                 className="product-image"
